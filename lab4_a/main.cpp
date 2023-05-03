f6f57e9b8ea327ad66e53830dd828f7f4bd2fb8d
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2023-05-03 22:38:05 +0300

    Update main.cpp

--- lab4_a/main.cpp
+++ lab4_a/main.cpp
@@ -18,7 +18,7 @@ private:
     int _groupNumber;
 public:
     Employee(const std::string& n, const int& y, const std::string& p, const int& g) : _name(n), _yearOfBirth(y), _position(p), _groupNumber(g) {}
-   
+
     int getBirthYear() const {
         return _yearOfBirth;
     }
@@ -39,7 +39,7 @@ public:
         employees.push_back(e);
     }
 
-    void printAllEmployees() const { 
+    void printAllEmployees() const {
         for (const auto& employee : employees) {
             employee->printInfo();
         }
@@ -50,7 +50,7 @@ public:
             if (e->getPosition() == "инженер") {
                 e->printInfo();
             }
-        });
+            });
     }
     void printAllNonPresidents() const {
         std::vector<Employee*> nonPresidents;
@@ -73,45 +73,45 @@ public:
         }
     }
 };
-    int main() {
-        setlocale(LC_ALL, "RUS");
-        Database db;
-        std::string _name, _position, _group;
-        int _yearOfBirth, _groupNumber;
-        std::string path = "bd.txt";
-        std::ifstream file;
-        file.open(path);
-        if (!file.is_open()) {
-            std::cout << "Error" << std::endl;
+int main() {
+    setlocale(LC_ALL, "RUS");
+    Database db;
+    std::string _name, _position, _group;
+    int _yearOfBirth, _groupNumber;
+    std::string path = "bd.txt";
+    std::ifstream file;
+    file.open(path);
+    if (!file.is_open()) {
+        std::cout << "Error" << std::endl;
+    }
+    else
+    {
+        std::cout << "Open!" << std::endl;
+        std::string line;
+        while (getline(file, line)) {
+            std::stringstream ss(line);
+            getline(ss, _name, ',');
+            ss >> _yearOfBirth >> std::ws;
+            getline(ss, _position, ',');
+            ss >> _group >> std::ws;
+            ss >> _groupNumber;
+            Employee* e = new Employee(_name, _yearOfBirth, _position, _groupNumber);
+            db.addEmployee(e);
         }
-        else
-        {
-            std::cout << "Open!" << std::endl;
-            std::string line;
-            while (getline(file, line)) {
-                std::stringstream ss(line);
-                getline(ss, _name, ',');
-                ss >> _yearOfBirth >> std::ws;
-                getline(ss, _position, ',');
-                getline(ss, _group, ' ');
-                ss >> _groupNumber;
-                Employee*e = new Employee(_name, _yearOfBirth, _position, _groupNumber);
-                db.addEmployee(e);
-            }
-            file.close();
-        }
-        std::cout << "Общая база данных:" << std::endl;
-        std::cout << "------------------------------------------------" << std::endl;
-        db.printAllEmployees();
-        std::cout << "------------------------------------------------" << std::endl;
-        std::cout << "Информация об инженерах:" << std::endl;
-        std::cout << std::endl;
-        std::cout << "------------------------------------------------" << std::endl;
-        db.printAllEngineers();
-        std::cout << "------------------------------------------------" << std::endl;
-        std::cout << "Информация о всех не председателях:" << std::endl;
-        std::cout << std::endl;
-        std::cout << "------------------------------------------------" << std::endl;
-        db.printAllNonPresidents();
-        return 0;
+        file.close();
+    }
+    std::cout << "Общая база данных:" << std::endl;
+    std::cout << "------------------------------------------------" << std::endl;
+    db.printAllEmployees();
+    std::cout << "------------------------------------------------" << std::endl;
+    std::cout << "Информация об инженерах:" << std::endl;
+    std::cout << std::endl;
+    std::cout << "------------------------------------------------" << std::endl;
+    db.printAllEngineers();
+    std::cout << "------------------------------------------------" << std::endl;
+    std::cout << "Информация о всех не председателях:" << std::endl;
+    std::cout << std::endl;
+    std::cout << "------------------------------------------------" << std::endl;
+    db.printAllNonPresidents();
+    return 0;
 }
